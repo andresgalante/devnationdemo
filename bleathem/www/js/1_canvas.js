ab338da12c7ef81586a54d683839773eb8397502
Brian Leathem <bleathem@gmail.com>
2016-06-17 09:15:39 -0700

    Used the getBoundingClientRect() API to correct for mousemove offsets

--- bleathem/www/js/1_canvas.js
+++ bleathem/www/js/1_canvas.js
@@ -3,7 +3,8 @@
 var _ = require('lodash');
 
 var canvas = document.getElementById('source'),
-    context = canvas.getContext('2d');
+    context = canvas.getContext('2d'),
+    rect;
 
 var colortest = document.querySelector('.color-display'),
     colorvalue = document.querySelector('.color-value');
@@ -18,10 +19,13 @@ image.onload = event => {
 
   canvas.width=width; canvas.height=height;
   context.drawImage(image, 0, 0, width, height)
+  rect = canvas.getBoundingClientRect();
 };
 
 canvas.addEventListener('mousemove', event => {
-  let imageData = context.getImageData(event.clientX, event.clientY, 1, 1);
+  let x = event.clientX - rect.left,
+      y = event.clientY - rect.top;
+  let imageData = context.getImageData(x, y, 1, 1);
   let [r,g,b, a] = imageData.data;
   colorvalue.innerText = `rgba(${r}, ${g}, ${b}, ${a})`
   colortest.style.background = `rgba(${r}, ${g}, ${b}, ${a})`;
