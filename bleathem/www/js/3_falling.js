93885016e01eb021a5d6c80eb7b89a8b3c934345
Brian Leathem <bleathem@gmail.com>
2016-06-18 08:04:26 -0700

    Animation demo cleanup

--- bleathem/www/js/3_falling.js
+++ bleathem/www/js/3_falling.js
@@ -2,19 +2,20 @@
 
 var canvas = require('./modules/canvas'),
     mosaic = require('./modules/mosaic'),
-    shuffle = require('./modules/shuffle');
+    shuffle = require('./modules/shuffle'),
+    Rx = require('rx');
 
 canvas.init('1800')
   .then(mosaic.init)
   .then(tiles => {
     shuffle(tiles);
-    console.log(tiles.length);
-    tiles.forEach(tile => {
-      setTimeout(
-        () => mosaic.drawTile(tile),
-        Math.floor(Math.random() * 5000)
-      );
-    })
+    Rx.Observable.zip(
+      Rx.Observable.from(tiles).bufferWithCount(2),
+      Rx.Observable.interval(4),
+      (tiles, index) => tiles
+    )
+    .flatMap(tiles => tiles)
+    .subscribe(tile => mosaic.drawTile(tile));
   })
   .catch(function(err) {
     console.error(err.stack);
